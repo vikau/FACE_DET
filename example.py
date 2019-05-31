db7440c880ca2bf5453957a9cb0f61a246f170e7
vikau <talkwithvikashsingh@gmail.com>
2019-06-01 01:55:13 +0530

    second commit

---
+++ example.py
@@ -0,0 +1,7 @@
+import datetime
+filename=datetime.datetime.now()
+def create_file():
+    with open(filename.strftime("%Y-%m-%d-%H-%M")+".txt","w") as file:
+        file.write("")
+create_file()
+        
